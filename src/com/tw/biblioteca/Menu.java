fee433e5ab0aae903b3df09c140be7e53d79b69f
sophiag16 <sophia.g16@gmail.com>
2015-09-09 05:07:54 +0000

    added menu option for return book

--- src/com/tw/biblioteca/Menu.java
+++ src/com/tw/biblioteca/Menu.java
@@ -13,6 +13,7 @@ public class Menu {
         menuOptions.add("1. List Books");
         menuOptions.add("2. Quit");
         menuOptions.add("3. Checkout Book");
+        menuOptions.add("4. Return Book");
     }
 
     public int chosenValidOption() {
